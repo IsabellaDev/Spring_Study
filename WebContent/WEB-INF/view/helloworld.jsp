9d926f759ed8ad6ecf4791f9c1cb36c694d3ba28
Isabella Liu <isabellaliu77@gmail.com>
2022-03-13 16:42:18 -0400

    added form data to the model

--- WebContent/WEB-INF/view/helloworld.jsp
+++ WebContent/WEB-INF/view/helloworld.jsp
@@ -8,6 +8,10 @@ Hello world of Spring!
 <br><br>
 
 Student name: ${param.studentName }
+<br><br>
+
+The message is: ${message} 
+
 <br>
 This is super cool!
 </body>
--- src/springdemo/mvc/HellowWorldController.java
+++ src/springdemo/mvc/HellowWorldController.java
@@ -1,6 +1,9 @@
 package springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +17,24 @@ public class HellowWorldController {
 	//a controller method to process the HTML form
 	@RequestMapping("/processForm")
 	public String processForm() {
+		return "helloworld";
+	}
+	
+	// controller method to read form data and add data to model
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+		
+		// convert the data to all capitals
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Hey " + theName + "!";
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
 		return "helloworld";
 	}
 
