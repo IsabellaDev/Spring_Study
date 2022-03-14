b2e1a2b18db38ddf3f38a99ac52e058081417391
Isabella Liu <isabellaliu77@gmail.com>
2022-03-13 22:52:17 -0400

    added @RequestParam()

--- src/springdemo/mvc/HellowWorldController.java
+++ src/springdemo/mvc/HellowWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HellowWorldController {
@@ -38,4 +39,20 @@ public class HellowWorldController {
 		return "helloworld";
 	}
 
+	@RequestMapping("/processFormVersionThree")
+	public String letsShoutDude(@RequestParam("studentName") String theName, Model model) {
+//		// read the request parameter from the HTML form
+//		String theName = request.getParameter("studentName");
+//		
+		// convert the data to all capitals
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Hey this is the third version of fetch form data, " + theName + "!";
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
 }
